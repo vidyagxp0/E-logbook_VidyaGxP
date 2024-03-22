7a35d73456b1e813ccc4f6fbf6f8071242f81b6e
MK7841 <manish.khatriya@vidyagxp.com>
2024-03-22 13:03:34 +0530

    chnages on chart

--- src/pages/analytics/Analytics.jsx
+++ src/pages/analytics/Analytics.jsx
@@ -2,54 +2,87 @@ import { useEffect, useState } from "react";
 import HeaderTop from "../../components/Header/HeaderTop";
 import HeaderBottom from "../../components/Header/HeaderBottom";
 import Chart from "react-apexcharts";
-import './Analytics.css';
+import "./Analytics.css";
 
 export default function Analytics() {
   const [selectedOption, setSelectedOption] = useState("hourly");
   const [chartData, setChartData] = useState([]);
   const [chartCategories, setChartCategories] = useState([
-    "Jan", "Feb", "March", "April", "May", "June",
-    "July", "Aug", "Sept", "Oct", "Nov", "Dec"
+    "Jan",
+    "Feb",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
   ]);
 
   const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
   };
 
-
   useEffect(() => {
     // Fetch data for chart based on selected option
     if (selectedOption === "hourly") {
       setChartData([
         {
-          name: "CAPA",
-          data: [30, 40, 45, 50, 49, 60, 70, 91],
+          name: "differential pressure",
+          data: [0.25, 0.5, 0.75, 1, 1.5, 2,2.5,3,3.5,3.75,],
         },
       ]);
       setChartCategories([
-        "00:00", "01:00", "02:00", "03:00", "04:00", "05:00",
-        "06:00", "07:00"
+        "00:00  PM",
+        "01:00  PM",
+        "02:00  PM",
+        "03:00  PM",
+        "04:00  PM",
+        "05:00  PM",
+        "06:00  PM",
+        "07:00  PM",
+        "08:00  PM",
       ]);
     } else if (selectedOption === "day") {
       setChartData([
         {
-          name: "CAPA",
-          data: [50, 60, 65, 70, 69, 80, 90],
+          name: "differential pressure",
+          data: [0.25, 2.65, 0.75, 1,2.75,2,2.75],
         },
       ]);
       setChartCategories([
-        "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
       ]);
     } else if (selectedOption === "month") {
       setChartData([
         {
-          name: "CAPA",
-          data: [70, 80, 85, 90, 89, 100, 90, 95, 70, 80, 85,90],
+          name: "differential pressure",
+          data: [0,0.25, 2.65, 0.75,3.5, 1,2.75,2,2.75,3,3.25,2.2,3.75,],
         },
       ]);
       setChartCategories([
-        "Jan", "Feb", "March", "April", "May", "June",
-        "July", "Aug", "Sept", "Oct", "Nov", "Dec"
+        "Month",
+        "Jan",
+        "Feb",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "Aug",
+        "Sept",
+        "Oct",
+        "Nov",
+        "Dec",
       ]);
     }
   }, [selectedOption]);
@@ -58,7 +91,8 @@ export default function Analytics() {
     <div>
       <HeaderTop />
       <HeaderBottom />
-      <div className="graph-2" style={{display:"flex"}}>
+      <h2 className="heading">Differential Pressure Record</h2>
+      <div className="graph-2" style={{ display: "flex" }}>
         <div className="chart-analytics">
           <Chart
             options={{
@@ -81,7 +115,7 @@ export default function Analytics() {
                 categories: chartCategories,
               },
               yaxis: {
-                max: 100, // Change the maximum value according to your data
+                max: 5, // Change the maximum value according to your data
               },
               plotOptions: {
                 line: {
@@ -91,7 +125,7 @@ export default function Analytics() {
               annotations: {
                 yaxis: [
                   {
-                    y: 80,
+                    y: 2.6,
                     borderColor: "#FF0000",
                     label: {
                       borderColor: "#FF0000",
@@ -99,7 +133,21 @@ export default function Analytics() {
                         color: "#fff",
                         background: "#FF0000",
                       },
-                      text: "80%",
+                      text: "2.6",
+                      offsetX: -30,
+                      offsetY: -5,
+                    },
+                  },
+                  {
+                    y: 0.6,
+                    borderColor: "#FF0000",
+                    label: {
+                      borderColor: "#FF0000",
+                      style: {
+                        color: "#fff",
+                        background: "#FF0000",
+                      },
+                      text: "0.6",
                       offsetX: -30,
                       offsetY: -5,
                     },
@@ -113,25 +161,24 @@ export default function Analytics() {
             height={600}
           />
         </div>
-        <div className="chart-data" style={{width:"200px"}}>
-        <div className="group-input">
-          <label className="color-label">Filter By</label>
-          <div className="instruction">&nbsp;</div>
-          <select
-            className="form-control"
-            name="assign_to"
-            onChange={handleOptionChange}
-            value={selectedOption}
-          >
-            <option value="Select a value">Select a value</option>
-            <option value="hourly">Hour</option>
-            <option value="day">Day</option>
-            <option value="month">Month</option>
-          </select>
+        <div className="chart-data" style={{ width: "200px" }}>
+          <div className="group-input">
+            <label className="color-label">Filter By</label>
+            <div className="instruction">&nbsp;</div>
+            <select
+              className="form-control"
+              name="assign_to"
+              onChange={handleOptionChange}
+              value={selectedOption}
+            >
+              <option value="Select a value">Select a value</option>
+              <option value="hourly">Hour</option>
+              <option value="day">Day</option>
+              <option value="month">Month</option>
+            </select>
+          </div>
         </div>
       </div>
-      </div>
-      
     </div>
   );
 }
