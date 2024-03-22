fae5ca66495853b93ca025ff60d642dcdd8a7ebb
pankaj9332 <pankaj.jat@vidyagxp.com>
2024-03-22 12:56:25 +0530

    changes in dpr

--- src/pages/configForms/DiffrentialPressureRecord/DiffrentialPressure.jsx
+++ src/pages/configForms/DiffrentialPressureRecord/DiffrentialPressure.jsx
@@ -192,14 +192,14 @@ export default function DiffrentialPressure() {
                     Details
                   </div>
                 </div>
-                <div className="analytics-btn">
+                {/* <div className="analytics-btn">
                   <button className="btn-print" onClick={() => navigate("/analytics")}>
                     Analytics
                   </button>
                   <button className="btn-print" onClick={() => {}}>
                     Print
                   </button>
-                </div>
+                </div> */}
               </div>
 
               {isSelectedGeneral === true ? (
@@ -479,10 +479,10 @@ export default function DiffrentialPressure() {
                     </tbody>
                   </table>
 
-                  <div className="group-input m">
+                  {/* <div className="group-input m">
                     <label> Review By :- </label>
-                  </div>
-
+                  </div> */}
+{/* 
                   <div className="group-input">
                     <label htmlFor="">Review Comments</label>
                     <textarea
@@ -491,7 +491,8 @@ export default function DiffrentialPressure() {
                         setDifferentialPRecord({ reviewComment: e.target.value });
                       }}
                     />
-                  </div>
+                  </div> */}
+
                   {/* Your JSX content */}
 
                   {/* <Grid
