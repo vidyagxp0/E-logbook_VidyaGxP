3ef587fd201060b3c6b21b4234a5db6290c12432
Farhan Khan <farhan.khan@vidyagxp.com>
2024-05-24 16:52:45 +0530

    Changes to edit user api

--- Frontend/src/pages/admin/AdminProcesses/AdminProcesses.jsx
+++ Frontend/src/pages/admin/AdminProcesses/AdminProcesses.jsx
@@ -1,17 +1,100 @@
-// import React, { useEffect, useState } from "react";
-// import "../adminDashboard/";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function AdminProcesses() {
-  
+  const [processes, setProcesses] = useState([]);
 
-    return (
-      <>
-        <div className="admin-dashboard">
-          <div className="main-content"></div>
+  useEffect(() => {
+    const url = "http://localhost:1000/process/get-processes"; // Assuming the endpoint is corrected to "/user/get-all-users"
+    axios
+      .get(url, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("admin-token")}`, // Include Bearer prefix with the token
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+        setProcesses(response.data.message); // Access the response data
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="admin-dashboard">
+        <div className="main-content"></div>
+        <div id="body-container" style={{ margin: "20px" }}>
+          <h3 style={{ textAlign: "center" }}>
+            <strong>Processes</strong>
+          </h3>
+          <br></br>
+          <hr />
+          {processes?.length === 0 ? (
+            <>No Processes Available</>
+          ) : (
+            <table
+              style={{
+                marginLeft: '150px',
+                width: "60%",
+                borderCollapse: "collapse",
+                backgroundColor: "#fff",
+                color: "#333",
+                borderRadius: "10px",
+                boxShadow: "0px 0px 10px 2px rgba(0, 0, 0, 0.1)",
+              }}
+            >
+              <thead>
+                <tr>
+                  <th
+                    style={{
+                      padding: "10px",
+                      borderBottom: "1px solid #ccc",
+                    }}
+                  >
+                    Process Number
+                  </th>
+                  <th
+                    style={{
+                      padding: "10px",
+                      borderBottom: "1px solid #ccc",
+                    }}
+                  >
+                    Process Name
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {processes.map((process, index) => (
+                  <tr key={index}>
+                    <td
+                      style={{
+                        padding: "10px",
+                        borderBottom: "1px solid #ccc",
+                      }}
+                    >
+                      {process.process_id}
+                    </td>
+                    <td
+                      style={{
+                        padding: "10px",
+                        borderBottom: "1px solid #ccc",
+                      }}
+                    >
+                      {process.process}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
         </div>
-      </>
-    );
-  }
-  
-  export default AdminProcesses;
+      </div>
+    </>
+  );
+}
+
+export default AdminProcesses;
+
   
--- Frontend/src/pages/admin/AdminSites/AdminSites.jsx
+++ Frontend/src/pages/admin/AdminSites/AdminSites.jsx
@@ -1,13 +1,95 @@
-// import React, { useEffect, useState } from "react";
-// import "../adminDashboard/";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function AdminSites() {
-  
+  const [sites, setSites] = useState([]);
+
+  useEffect(() => {
+    const url = "http://localhost:1000/site/get-sites"; // Assuming the endpoint is corrected to "/user/get-all-users"
+    axios
+      .get(url, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("admin-token")}`, // Include Bearer prefix with the token
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+        setSites(response.data.message); // Access the response data
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
   return (
     <>
       <div className="admin-dashboard">
         <div className="main-content"></div>
+        <div id="body-container" style={{ margin: "20px" }}>
+          <h3 style={{ textAlign: "center" }}>
+            <strong>Sites</strong>
+          </h3>
+          <br></br>
+          <hr />
+          {sites?.length === 0 ? (
+            <>No Sites Available</>
+          ) : (
+            <table
+              style={{
+                marginLeft: '150px',
+                width: "60%",
+                borderCollapse: "collapse",
+                backgroundColor: "#fff",
+                color: "#333",
+                borderRadius: "10px",
+                boxShadow: "0px 0px 10px 2px rgba(0, 0, 0, 0.1)",
+              }}
+            >
+              <thead>
+                <tr>
+                  <th
+                    style={{
+                      padding: "10px",
+                      borderBottom: "1px solid #ccc",
+                    }}
+                  >
+                    Site Number
+                  </th>
+                  <th
+                    style={{
+                      padding: "10px",
+                      borderBottom: "1px solid #ccc",
+                    }}
+                  >
+                    Site Name
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {sites.map((site, index) => (
+                  <tr key={index}>
+                    <td
+                      style={{
+                        padding: "10px",
+                        borderBottom: "1px solid #ccc",
+                      }}
+                    >
+                      {site.site_id}
+                    </td>
+                    <td
+                      style={{
+                        padding: "10px",
+                        borderBottom: "1px solid #ccc",
+                      }}
+                    >
+                      {site.site}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </div>
       </div>
     </>
   );
