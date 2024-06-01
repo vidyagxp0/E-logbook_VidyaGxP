2b9acbf86b478efea5402286034ef3fafaf42928
Farhan Khan <farhan.khan@vidyagxp.com>
2024-06-01 10:41:52 +0530

    Updated protected routes

--- Frontend/src/components/protectedRoutes/protectedUserRoutes.jsx
+++ Frontend/src/components/protectedRoutes/protectedUserRoutes.jsx
@@ -1,10 +1,32 @@
 // ProtectedRoute.jsx
-import React from "react";
-import { Route, Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
+// eslint-disable-next-line react/prop-types
 const ProtectedRoute = ({ element: Component }) => {
-  const isAuthenticated = !!localStorage.getItem("user-token"); // Replace with your actual auth check
-  return isAuthenticated ? Component : <Navigate to="/" replace />;
+  const token = localStorage.getItem("user-token");
+  const isAuthenticated = !!token;
+
+  const isTokenExpired = (token) => {
+    if (!token) return true;
+
+    try {
+      const decodedToken = jwtDecode(token);
+      const currentTime = Date.now() / 1000; // Convert to seconds
+      return decodedToken.exp < currentTime;
+    } catch (error) {
+      console.error("Failed to decode token", error);
+      return true;
+    }
+  };
+
+  if (!isAuthenticated || isTokenExpired(token)) {
+    localStorage.removeItem("user-token"); // Remove expired token
+    localStorage.removeItem("user-details"); // Remove expired token
+    return <Navigate to="/" replace />;
+  }
+
+  return Component;
 };
 
 export default ProtectedRoute;
