93708cdd0b351296af4a819348189bbf6bb1b567
Anshul <139344452+Mranshul1997@users.noreply.github.com>
2024-11-19 14:24:08 +0530

    corrected MR

--- Frontend/src/pages/configForms/MediaRecord/MediaRecord.jsx
+++ Frontend/src/pages/configForms/MediaRecord/MediaRecord.jsx
@@ -246,7 +246,14 @@ const MediaRecord = () => {
         </div>
         <div className="document-form">
           <div className="details-form-data">
-         d
+           <div className="sop-type-header">
+              <div className="logo">
+                <img src="/vidyalogo2.png" alt="..." />
+              </div>
+              <div className="main-head">
+                <div>VidyaGxP Private Limited</div>
+              </div>
+            </div>
             <div className="sub-head-2">Media Records</div>
 
             <div className="outerDiv4">
