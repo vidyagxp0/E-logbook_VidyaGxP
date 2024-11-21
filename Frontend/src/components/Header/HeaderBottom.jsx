f090c8c4f6b0cbf88c9814ba7e756cbf1da156b0
Rupesh Patil <rp61223456@gmail.com>
2024-11-21 15:57:22 +0530

    bottom header fixed

--- Frontend/src/components/Header/HeaderBottom.jsx
+++ Frontend/src/components/Header/HeaderBottom.jsx
@@ -28,7 +28,7 @@ function HeaderBottom() {
               <option value="temperature_records">Temperature Records</option>
             </select>
           </div> */}
-            <div className="tabs flex items-center justify-center space-x-6">
+            <div className="tabs flex items-center justify-center space-x-6 ml-5">
               <Link
                 to="/dashboard"
                 className={`tab py-2 px-4 rounded-md  ${
@@ -41,8 +41,8 @@ function HeaderBottom() {
               </Link>
               <Link
                 to="/effectiveElogs"
-                className={`tab py-2 px-4 rounded-md  ${
-                  window.location.pathname === "/dashboard2"
+                className={`tab py-2 px-3 min-w-fit rounded-md  ${
+                  window.location.pathname === "/effectiveElogs"
                     ? "bg-blue-600 text-white"
                     : "bg-white text-blue-500 border border-blue-500 "
                 } hover:bg-blue-600 transition duration-300  hover:text-white `}
@@ -52,7 +52,7 @@ function HeaderBottom() {
               <Link
                 to="/analytics2"
                 className={`tab py-2 px-4 rounded-md  ${
-                  window.location.pathname === "/dashboard2"
+                  window.location.pathname === "/analytics2"
                     ? "bg-blue-600 text-white"
                     : "bg-white text-blue-500 border border-blue-500 "
                 } hover:bg-blue-600 transition duration-300  hover:text-white `}
