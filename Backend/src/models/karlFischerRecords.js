cd5537084c4e976cb2643341a60c0ec3c78d030b
Tushal Patel <146264436+Tushalp@users.noreply.github.com>
2025-05-27 16:22:42 +0530

    add field reviewed by in jkarl fischer records

--- Backend/src/models/karlFischerRecords.js
+++ Backend/src/models/karlFischerRecords.js
@@ -39,6 +39,9 @@ const karlFischerRecord = sequelize.define(
     checked_by: {
       type: DataTypes.STRING,
     },
+    reviewed_by: {
+      type: DataTypes.STRING,
+    },
     remarks: {
       type: DataTypes.TEXT,
     },
