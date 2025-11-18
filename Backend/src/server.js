4cc8c81edf64e5a98f3ebe1682d3ff3aa1ea6e5e
Tushalp <146264436+Tushalp@users.noreply.github.com>
2025-11-18 20:40:20 +0530

    Add New Process UV-VIS Spectrophotometer

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -11,6 +11,7 @@ const dispensingOfMaterialRoutes = require("./routes/dispensingOfMaterial");
 const operationOfSterlizerRoutes = require("./routes/operationOfSterlizer");
 const analyticalBalanceRoutes = require("./routes/AnalyticalBalance")
 const opAndCalParamterRoute = require("./routes/OpAndCalProcessRoute")
+const OpAndCalUvVisRoute = require("./routes/OpAndCalUvVisRoute")
 const karlFischerRoutes = require("./routes/karlFischer")
 const hplcRoutes = require("./routes/hplcRoutes")
 const vidyagxpFeedback = require("./config/vidyagxp_feedback");
@@ -63,6 +64,7 @@ app.use("/dispensing-material", dispensingOfMaterialRoutes);
 app.use("/site", siteRoutes);
 app.use("/analytical-balance",analyticalBalanceRoutes);
 app.use("/op-and-calParameter",opAndCalParamterRoute);
+app.use("/uv-vis-calib",OpAndCalUvVisRoute);
 app.use("/karl-fischer",karlFischerRoutes);
 app.use("/hplc",hplcRoutes)
 app.use(express.static(path.join(__dirname, "documents")));
