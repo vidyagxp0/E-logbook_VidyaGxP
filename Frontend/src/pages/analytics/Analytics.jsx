b78b68cf8bf58237caf299dbe11cef5e406c6f75
Tushalp <146264436+Tushalp@users.noreply.github.com>
2025-11-19 11:00:05 +0530

    add Analytics

--- Frontend/src/pages/analytics/Analytics.jsx
+++ Frontend/src/pages/analytics/Analytics.jsx
@@ -79,7 +79,7 @@ export default function Analytics() {
       <HeaderTop />
       <HeaderBottom />
 
-      {/* <div style={{ display: "flex", gap: "1rem", alignItems: "center", padding: "1rem" }}>
+      <div style={{ display: "flex", gap: "1rem", alignItems: "center", padding: "1rem" }}>
         <Select
           placeholder="Select Chart Type"
           style={{
@@ -98,10 +98,10 @@ export default function Analytics() {
         />
 
         <RangeDatePicker handleSelectedDate={handleSelectedDate} />
-      </div> */}
+      </div>
 
       {/* Only render the chart when both chart type and date range are selected */}
-      {/* {selectedChartType && selectedDateRange[0] && selectedDateRange[1] && graphData.length > 0 && (
+      {selectedChartType && selectedDateRange[0] && selectedDateRange[1] && graphData.length > 0 && (
         <DynamicLineChart
           heading={
             selectedChartType === "differential_pressure"
@@ -117,12 +117,7 @@ export default function Analytics() {
           highchartData={graphData}
           fromCPP={true}
         />
-      )} */}
-      <div className="flex items-center justify-center h-screen animate-fadeIn">
-  <h1 className="text-4xl font-bold text-center text-blue-700">
-    Will be available in the final version
-  </h1>
-</div>
+      )}
     </div>
   );
 }
