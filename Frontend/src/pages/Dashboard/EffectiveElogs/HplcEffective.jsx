bb01ad146b2a120264b771fe8328b105e217e10b
Rupesh Patil <108573358+rupeshpatil02@users.noreply.github.com>
2025-11-19 14:42:50 +0530

    Prevent adding new row if current row incomplete

--- Frontend/src/pages/Dashboard/EffectiveElogs/HplcEffective.jsx
+++ Frontend/src/pages/Dashboard/EffectiveElogs/HplcEffective.jsx
@@ -260,6 +260,32 @@ const HplcEffective = () => {
   }, [location.state]);
 
   const addRow = () => {
+
+      const records = editData?.hplcRecords || [];
+
+  // Function to check if a row is filled
+  const isRowComplete = (row) => {
+    return (
+      row.sample_name?.trim() !== "" && 
+      row.reg_no?.trim() !== "" && 
+      row.method_used?.trim() !== "" && 
+      row.parameter_or_activity?.trim() !== "" && 
+      row.column_no?.trim() !== "" && 
+      row.start_time?.trim() !== "" && 
+      row.reviewed_by !== null
+    );
+  };
+
+  // 1️⃣ Check if there is at least 1 row
+  if (records.length > 0) {
+    const lastRow = records[records.length - 1];
+
+    // 2️⃣ If last row is empty → block adding a new row
+    if (!isRowComplete(lastRow)) {
+      toast.warn("Please fill the current row before adding a new one.");
+      return;
+    }
+  }
     if (
       userDetails.roles[0].role_id === 1 ||
       userDetails.roles[0].role_id === 5
