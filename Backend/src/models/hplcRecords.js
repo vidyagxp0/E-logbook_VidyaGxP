1b28049d29ab492a6aeacd5220cc52a2b42b011f
Tushalp <146264436+Tushalp@users.noreply.github.com>
2025-11-26 14:26:18 +0530

    Add performance fields to HPLC records and audit trail
    Introduces new fields related to performance (performance, performanceStartTime, performanceEndTime, performanceEndDate, performanceEndDateTime, performanceRemark) in the hplcRecords model. Updates InsertHPLC and EditHPLC controllers to handle these fields and logs their creation in the audit trail.

--- Backend/src/models/hplcRecords.js
+++ Backend/src/models/hplcRecords.js
@@ -80,6 +80,24 @@ const hplcRecord = sequelize.define(
     remarksSubType: {
       type: DataTypes.STRING,
     },
+    performance: {
+      type: DataTypes.STRING,
+    },
+    performanceStartTime: {
+      type: DataTypes.STRING,
+    },
+    performanceEndTime: {
+      type: DataTypes.STRING,
+    },
+    performanceEndDate: {
+      type: DataTypes.STRING,
+    },
+    performanceEndDateTime: {
+      type: DataTypes.STRING,
+    },
+    performanceRemark: {
+      type: DataTypes.STRING,
+    },
     status: {
       type: DataTypes.STRING
     }
