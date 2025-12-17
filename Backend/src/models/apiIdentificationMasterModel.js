470a79f63bb7a915748932d41891a3dd35684b09
Tushalp <146264436+Tushalp@users.noreply.github.com>
2025-12-17 12:37:04 +0530

    minor name change

--- Backend/src/models/apiIdentificationMasterModel.js
+++ Backend/src/models/apiIdentificationMasterModel.js
@@ -10,7 +10,7 @@ const identificationMaster = sequelize.define(
       primaryKey: true,
 
     },
-     siteMasterData: { 
+     apiIdentificationData: { 
       type: DataTypes.JSON, 
       allowNull: false 
     },
